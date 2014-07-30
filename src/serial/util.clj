a3bb1b14522e99e992723c2cd31ff0b596b9cfa2
Blake Jakopovic <blake.jakopovic@gmail.com>
2014-07-30 10:38:36 +1000

    Added arduino port detection util methods

--- src/serial/util.clj
+++ src/serial/util.clj
@@ -8,3 +8,20 @@
     (when ports
       (println (.getName (first ports)))
       (recur (next ports)))))
+
+(defn- substring? [sub st]
+  (not= (.indexOf st sub) -1))
+
+(defn- arduino-port?
+  "Compares port name with known arduino port formats"
+  [port-name]
+  (or
+    (substring? "tty.usbmodem" port-name)    ;; Uno or Mega 2560
+    (substring? "tty.usbserial" port-name))) ;; Older boards
+
+(defn detect-arduino-port
+  "Returns the first arduino serial port based on port
+   name, or nil"
+  []
+  (first (filter arduino-port?
+                 (map #(.getName %) (port-ids)))))
